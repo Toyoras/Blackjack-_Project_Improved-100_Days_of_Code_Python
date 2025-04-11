b3f400d1cec862867c175694babb39cc7639dcdc
Toyoras <129657915+Toyoras@users.noreply.github.com>
2025-04-11 14:06:29 +0200

    Update main.py
    adding try except on useful global functions

--- main.py
+++ main.py
@@ -165,35 +165,44 @@ class Game:
 def get_valid_input_text(text):
     """Check that text inputs are valid"""
     while True:
-        user_input = input(text).lower()
-        if user_input == "yes":
-            return True
-        elif user_input == "no":
-            return False
-        else:
-            print("Typo Error, please type 'yes' or 'no'")
+        try:
+            user_input = input(text).lower()
+            if user_input == "yes":
+                return True
+            elif user_input == "no":
+                return False
+            else:
+                print("Typo Error, please type 'yes' or 'no'")
+        except Exception as e:
+            print(f"An error occurred: {e}, please enter 'yes' or 'no'")
 
 def get_valid_balance(bank_amount_text):
     """Check that balance inputs are valid"""
     while True:
-        balance = int(float(input(bank_amount_text)))
-        if balance <= 0:
-            print("The bid must be a positive number. Please try again.")
-        else:
-            print(f"Your amount of ${balance} is accepted.")
-            return balance
+        try:
+            balance = int(float(input(bank_amount_text)))
+            if balance <= 0:
+                print("The bid must be a positive number. Please try again.")
+            else:
+                print(f"Your amount of ${balance} is accepted.")
+                return balance
+        except Exception as e:
+            print(f"An error occurred: {e}, please enter a valid amount")
 
 def get_valid_bid(balance, bid_amount_text):
     """Check that bid inputs are valid"""
     while True:
-        bid_check = int(float(input(bid_amount_text)))
-        if bid_check <= 0:
-            print("The bid must be a positive number. Please try again.")
-        elif bid_check > balance :
-            print(f"You cannot bid more than your bank amount (${balance}). Try again.")
-        else:
-            print(f"Your bid of ${bid_check} is accepted.")
-            return bid_check 
+        try:
+            bid_check = int(float(input(bid_amount_text)))
+            if bid_check <= 0:
+                print("The bid must be a positive number. Please try again.")
+            elif bid_check > balance :
+                print(f"You cannot bid more than your bank amount (${balance}). Try again.")
+            else:
+                print(f"Your bid of ${bid_check} is accepted.")
+                return bid_check 
+        except Exception as e:
+            print(f"An error occurred: {e}, please enter a valid amount")
 
 # ----- Main -----
 def main():
